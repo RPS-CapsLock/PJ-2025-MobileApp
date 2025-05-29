45baeb490405d0fab5ac80b4e27356f86407c7ee
janmilosic <milosic.jan@gmail.com>
2025-05-29 19:09:58 +0200

    Added MediaPlayer to play decoded sound

--- app/src/main/java/com/example/clapp/AudioUtil.kt
+++ app/src/main/java/com/example/clapp/AudioUtil.kt
@@ -1,5 +1,6 @@
 package com.example.clapp
 
+import android.media.MediaPlayer
 import android.util.Base64
 import java.io.File
 import java.io.FileOutputStream
@@ -10,6 +11,22 @@ object AudioUtil {
         return try {
             val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
             FileOutputStream(outputFile).use { it.write(decodedBytes) }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    fun playMp3File(file: File): Boolean {
+        return try {
+            val mediaPlayer = MediaPlayer()
+            mediaPlayer.setDataSource(file.absolutePath)
+            mediaPlayer.setOnCompletionListener {
+                it.release()
+            }
+            mediaPlayer.prepare()
+            mediaPlayer.start()
             true
         } catch (e: Exception) {
             e.printStackTrace()
