add3cf659e053ce9e4f48696ccf4594d6a8878a6
mimco <klajnsek.mitja@gmail.com>
2025-06-01 13:14:52 +0200

    added the login functionality to the LoginUtil class PCB-37

--- app/src/main/java/com/example/clapp/loginUtil/LoginUtil.kt
+++ app/src/main/java/com/example/clapp/loginUtil/LoginUtil.kt
@@ -15,5 +15,50 @@ import org.json.JSONObject
 import java.io.IOException
 
 object LoginUtil {
+    var username: String = ""
+        private set;
+    var password: String = ""
+        private set;
+    private val client = OkHttpClient.Builder()
+        .cookieJar(object : CookieJar {
+            private val cookieStore = mutableMapOf<HttpUrl, List<Cookie>>()
+            override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+                cookieStore[url] = cookies
+            }
+            override fun loadForRequest(url: HttpUrl): List<Cookie> {
+                return cookieStore[url] ?: emptyList()
+            }
+        })
+        .build()
+    fun sendLoginRequest(username: String, password: String) {
+        val json = JSONObject().apply {
+            put("username", username)
+            put("password", password)
+        }
 
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = json.toString().toRequestBody(mediaType)
+
+        val request = Request.Builder()
+            .url("http://10.0.2.2:3001/users/login")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("LoginResponse", "Request failed: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseData = response.body?.string()
+                    Log.d("LoginResponse", "Response: $responseData")
+                } else {
+                    Log.e("LoginResponse", "Error ${response.code}: ${response.message}")
+                    val errorBody = response.body?.string()
+                    Log.e("LoginResponse", "Error body: $errorBody")
+                }
+            }
+        })
+    }
 }
