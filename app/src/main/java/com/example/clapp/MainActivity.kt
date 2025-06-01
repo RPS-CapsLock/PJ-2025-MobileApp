957894d5cefb074e119e4683571dfa7c7f23cc2b
RokAnzel <roki.anzel@gmail.com>
2025-06-01 14:40:00 +0200

    Added back button functionality to MixingActivity

--- app/src/main/java/com/example/clapp/MainActivity.kt
+++ app/src/main/java/com/example/clapp/MainActivity.kt
@@ -7,22 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.clapp.ui.theme.CLAppTheme
 import android.view.LayoutInflater
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MixingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mixing_cocktails)
+
+        val backButton = findViewById<android.widget.Button>(R.id.Back_button)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
 
@@ -32,9 +34,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CLAppTheme {
-                Scaffold(modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF001F54))
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF001F54))
                 ) { innerPadding ->
                     AndroidView(
                         factory = { context ->
@@ -45,6 +48,7 @@ class MainActivity : ComponentActivity() {
                                 val intent = android.content.Intent(context, QRscanner::class.java)
                                 context.startActivity(intent)
                             }
+
                             val mixingButton = view.findViewById<android.widget.Button>(R.id.Mixing_button)
                             mixingButton.setOnClickListener {
                                 val intent = android.content.Intent(context, MixingActivity::class.java)
